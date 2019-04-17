d4f512d8e6fc1bd3170abe96694fac1228e1514d
Kirtish Dhande <aniket.dhande@gmail.com>
2019-04-17 21:16:39 +0530

    Create Q3.java
    Assignment 1 Question 3

---
+++ JAVA/ASS1/Q3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class QQQ{
+	public static void main(String[] ss){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("First unlucky number");
+		int num1=sc.nextInt();
+		System.out.println("Second unlucky number");
+		int num2=sc.nextInt();
+		if (num1>num2){
+			System.out.println("Ka karto aahe apan he");
+		}
+		else {
+			System.out.println("Kyu hai hum yaha");
+		}
+	}
+}
