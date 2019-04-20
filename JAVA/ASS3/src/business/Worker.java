ae7c35657b7fd4a6aad6af093b469cc2a2091a59
Kirtish Dhande <aniket.dhande@gmail.com>
2019-04-20 21:42:52 +0530

    Create Worker.java

---
+++ JAVA/ASS3/src/business/Worker.java
@@ -0,0 +1,30 @@
+package business;
+
+public class Worker extends Emp{
+	
+	private int hrsWorked;
+	private double hrsRate;
+
+	public Worker(String name, String email, int deptId, double basic, int hrsWorked, double hrsRate) {
+		super(name, email, deptId, basic);
+		this.hrsWorked=hrsWorked;
+		this.hrsRate=hrsRate;
+	}
+	
+	@Override
+	public double computeNetSalary() {
+		return getBasic()+(hrsRate*hrsWorked);
+	}
+	
+	@Override
+	public String toString() {
+		return "Worker--\n"+ super.toString()+" | HrsWorked-"+hrsWorked+" | HrsRate-"+hrsRate;
+	}
+
+	public double getHrsRate() {
+		return hrsRate;
+	}
+
+	
+
+}
