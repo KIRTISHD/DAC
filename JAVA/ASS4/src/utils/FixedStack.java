b303db75136b1ab912e1c0c6e69f2be47133ea2d
Kirtish Dhande <aniket.dhande@gmail.com>
2019-04-20 21:44:40 +0530

    Create FixedStack.java

---
+++ JAVA/ASS4/src/utils/FixedStack.java
@@ -0,0 +1,26 @@
+package utils;
+
+import business.Emp;
+
+public class FixedStack implements Stack {
+
+	public static int count;
+	
+	
+	@Override
+	public void push(Emp e) {
+		if (count<=STACK_SIZE) {
+			
+		}
+		else {
+			
+		}
+	}
+
+	@Override
+	public void pop() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
