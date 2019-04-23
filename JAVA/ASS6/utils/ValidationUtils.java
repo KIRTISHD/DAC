8cdbcf6b3756f184c3b1d2f2de6d87e7b2602099
Kirtish Dhande <aniket.dhande@gmail.com>
2019-04-23 22:04:13 +0530

    Create ValidationUtils.java

---
+++ JAVA/ASS6/utils/ValidationUtils.java
@@ -0,0 +1,22 @@
+package utils;
+
+
+public class ValidationUtils {
+	
+	public static void checkEmail(String st) throws Exception{
+		if (st.length() < 5 || st.length()>10) {
+			throw new ThrowErrorClass("Email Length not within limits");
+		}
+		if (!st.contains("@")) {
+			throw new ThrowErrorClass("Email does not have @");
+		}
+		
+	}
+	
+	public static void checkBasic(double sal) throws Exception{
+		if (sal %1000 != 0) {
+			throw new ThrowErrorClass("Basic is not a multiple of 1000");
+		}
+	}
+
+}
