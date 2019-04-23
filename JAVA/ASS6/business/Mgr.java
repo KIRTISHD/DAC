b5e874ea3693ce8b335bd8360529174dafa2cde5
Kirtish Dhande <aniket.dhande@gmail.com>
2019-04-23 22:02:31 +0530

    Create Mgr.java

---
+++ JAVA/ASS6/business/Mgr.java
@@ -0,0 +1,29 @@
+package business;
+
+public class Mgr extends Emp{
+
+	public Mgr(String name, String email, int deptId, double basic, double perfBonus) {
+		super(name, email, deptId, basic);
+		this.perfBonus=perfBonus;
+		// TODO Auto-generated constructor stub
+	}
+
+	private double perfBonus;
+	
+	@Override
+	public double computeNetSalary() {
+		return perfBonus*getBasic();
+	}
+
+	public double getPerfBonus() {
+		return perfBonus;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "Manager--\n"+ super.toString()+" | perfBonus-"+perfBonus;
+	}
+
+
+}
