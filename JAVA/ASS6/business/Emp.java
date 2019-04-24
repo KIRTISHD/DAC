b6001f80703bfe1118fedf8cd119cbe0895b9265
Kirtish Dhande <aniket.dhande@gmail.com>
2019-04-24 19:21:21 +0530

    Update Emp.java

--- JAVA/ASS6/business/Emp.java
+++ JAVA/ASS6/business/Emp.java
@@ -4,7 +4,7 @@ public abstract class Emp {
 	private int id;
 	private String email;
 	private String name;
-	private int deptId;
+	private String dept;
 	private double basic;
 	
 	public double getBasic() {
@@ -16,16 +16,16 @@ public abstract class Emp {
 	static {
 		int_gen =100;
 	}
-	public Emp(String name,String email,int deptId,double basic){
+	public Emp(String name,String email,String dept,double basic){
 		this.id=int_gen++;
 		this.name=name;
 		this.email=email;
-		this.deptId=deptId;
+		this.dept=dept;
 		this.basic=basic;
 	}
 
 	public String toString() {
-		return "Id-"+id+" | Name-"+name+" | Email-"+ email+ " | deptId-"+deptId+" | Basic-"+ basic;
+		return "Id-"+id+" | Name-"+name+" | Email-"+ email+ " | dept-"+dept+" | Basic-"+ basic;
 	}
 	
 	public void setBasic(double basic) {
