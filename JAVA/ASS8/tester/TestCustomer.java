2b6115e1d42920306004a9c4316a9e88a86f6c2c
Kirtish Dhande <aniket.dhande@gmail.com>
2019-04-25 21:19:14 +0530

    Create TestCustomer.java

---
+++ JAVA/ASS8/tester/TestCustomer.java
@@ -0,0 +1,14 @@
+package tester;
+
+import java.util.Scanner;
+
+public class TestCustomer {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		if(sc!=null)
+			sc.close();
+	}
+
+}
