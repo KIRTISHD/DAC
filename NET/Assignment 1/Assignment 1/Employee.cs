2bf0961e94da6f5f01e64cec564f8db40e08a75a
KIRTISHD <aniket.dhande@gmail.com>
2019-06-19 22:26:24 +0530

    Adding Assignment 2
    with gitignore

---
+++ NET/Assignment 1/Assignment 1/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    public class Employee
+    {
+        private int EmployeeID;
+        private string EmployeeName;
+        private DateTime EmployeeDOJ;
+        private double EmployeeBasicSalary;
+
+        public Employee(int EmployeeID, string EmployeeName, DateTime EmployeeDOJ, double EmployeeBasicSalary)
+        {
+            this.EmployeeID = EmployeeID;
+            this.EmployeeName = EmployeeName;
+            this.EmployeeDOJ = EmployeeDOJ;
+            this.EmployeeBasicSalary = EmployeeBasicSalary;
+        }
+
+        public void getDetails()
+        {
+            Console.WriteLine("Employee id is->" + EmployeeID);
+            Console.WriteLine("Employee name is->" + EmployeeName);
+            Console.WriteLine("Employee DOJ is->" + EmployeeDOJ);
+            Console.WriteLine("Employee Basic Salary is->" + EmployeeBasicSalary);
+        }
+
+        public void GetExperience()
+        {
+            DateTime today = DateTime.Now;
+            TimeSpan st = today - EmployeeDOJ;
+            Console.WriteLine("Experience is ->" + st.Days);
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Employee Basic Salary is-" + EmployeeBasicSalary);
+        }
+    }
+
+    class EmployeeCall
+    {
+       /* public static void Main(String[] ss)
+        {
+            Employee e1 = new Employee(10,"NITYA",Convert.ToDateTime("10/10/1992"),90000);
+            e1.getDetails();
+            e1.GetExperience();
+            e1.GetSalary();
+            Console.ReadKey();
+        } */
+    }
+}
+
---
+++ NET/Assignment 1/Assignment 1/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    public class Student
+    {
+        private int StudentID;
+        private string StudentName;
+        private DateTime StudentDOB;
+        private String StudentCity;
+
+        public Student(int StudentID, string StudentName, DateTime StudentDOB, String StudentCity)
+        {
+            this.StudentID = StudentID;
+            this.StudentName = StudentName;
+            this.StudentDOB = StudentDOB;
+            this.StudentCity = StudentCity;
+        }
+
+        public void getDetails()
+        {
+            Console.WriteLine("Student id is-" + StudentID);
+            Console.WriteLine("Student name is-" + StudentName);
+            Console.WriteLine("Student DOB is-" + StudentDOB.Date);
+            Console.WriteLine("Student City is-" + StudentCity);
+        }
+
+        public void GetAge()
+        {
+            DateTime today = DateTime.Now;
+            Console.WriteLine("Age is -"+today.CompareTo(StudentDOB));
+        }
+    }
+
+    class StudentCall
+    {
+        public static void Main(String[] ss)
+        {
+            Student st = new Student(10,"Kirtya",Convert.ToDateTime("10/10/1994"),"PUNE");
+            st.GetAge();
+            st.getDetails();
+            Console.ReadKey();
+
+        }
+    }
+}
