e5a469aa3eb9dbf64f6f25e7e132c1877e4d6045
KIRTISHD <aniket.dhande@gmail.com>
2019-06-19 10:28:33 +0530

    NET Assignments

---
+++ NET/Assignment 1/Assignment 1/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    public class Book
+    {
+        private int BookID;
+        private string BookName;
+        private double BookPrice;
+        private string AuthorName;
+
+        public Book(int BookID, string BookName, double BookPrice, string AuthorName)
+        {
+            this.BookID = BookID;
+            this.BookName = BookName;
+            this.BookPrice = BookPrice;
+            this.AuthorName = AuthorName;
+        }
+
+        public void GetDetails()
+        {
+            Console.WriteLine("Book Id- " + BookID);
+            Console.WriteLine("Book Name- " + BookName);
+            Console.WriteLine("Book Price- " + BookPrice);
+            Console.WriteLine("Author Name- " + AuthorName);
+        }
+
+
+
+    }
+
+    class BookClass
+    {
+       /* {
+            Book bk = new Book(1,"10 Mistakes for taking CDAC",6969,"Donald Tatya");
+            bk.GetDetails();
+            Console.ReadKey();
+        }*/
+    }
+}
---
+++ NET/Calculator/Calculator/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static int add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        static int multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        static double divide(int x, int y)
+        {
+            return Convert.ToDouble(x / y);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number-");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number-");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the operation you want to perform + - / *");
+            var op = Console.ReadLine();
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine("Addition of two numbers is->" + add(x, y));
+                    break;
+                case "-":
+                    Console.WriteLine("Subtraction of two numbers is->" + subtract(x, y));
+                    break;
+                case "*":
+                    if (y == 0 || x==0)
+                    {
+                        Console.WriteLine("One of the numbers is 0");
+                        Console.WriteLine("Badal salya");
+                        break;
+                    }
+                    Console.WriteLine("Multiplication of two numbers is->" + multiply(x, y));
+                    break;
+                case "/":
+                    if (y == 0)
+                    {
+                        Console.WriteLine("Denominator cannot be 0");
+                        Console.WriteLine("Kal cha aptitude class nahi kela ka?");
+                        break;
+                    }
+                    Console.WriteLine("Division of two numbers is->" + divide(x, y));
+                    break;
+                default :
+                    Console.WriteLine("Java kar ja");
+                    break;
+
+            }
+
+            Console.WriteLine("Ganya program chalala");
+            Console.ReadLine();
+        }
+    }
+}
